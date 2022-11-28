8279628ffa974ea1a5b3bb5663ec033bdfe322c5
jnievehz <jimwel.nieves@stu.ocsb.ca>
2022-11-28 14:04:11 -0500

    complete counting chicken and profit

--- src/Chickens01.java
+++ src/Chickens01.java
@@ -1,7 +1,20 @@
 public class Chickens01 {
     public static void main(String[] args) {
         //Put your code here
-        
-        System.out.println(totalEggs);
+    	
+    
+    	   int totalEggs = 0;
+           int totalChickens = 3;
+           int x = 5;
+           int y = x + 5;
+           System.out.println(totalEggs);
+           System.out.println(totalChickens);
+           System.out.println(x);
+           System.out.println(y);
+           System.out.println(++y);
+           System.out.println(++x);
+           
+           
+
     }   
 }
--- src/Chickens02.java
+++ src/Chickens02.java
@@ -1,10 +1,39 @@
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
-
+        int totalEggs = 0;
+    int eggsPerChicken = 4;
+        int chickenCount = 8;
+        // MONDAY
+        totalEggs = eggsPerChicken * chickenCount;
+        // TUESDAY
+        totalEggs += eggsPerChicken * chickenCount;
+        //WEDNESDAY
+        chickenCount /= 2;
+        totalEggs += eggsPerChicken * chickenCount;
+       
+        double eggCount = 100;
+        double dailyAverage = 0;
+        double monthlyAverage = dailyAverage * 30;
+        double monthlyProfit = 0;
+       
+        //Monday
+        eggCount += 100; //Collect 100 eggs
+        //Tuesday
+        eggCount += 121; //Collect 121
+        //Wednesday
+        eggCount += 117; //Collect 117 eggs
+       
+        //Calculate Daily Average
+        dailyAverage = eggCount / 3;
         System.out.println("Daily Average:   " +dailyAverage);
+        //Calculate Monthly Average
+        monthlyAverage = dailyAverage * 30;
         System.out.println("Monthly Average: " +monthlyAverage);
+        //Calculate Monthly Profit
+        monthlyProfit = monthlyAverage * 0.18;
         System.out.println("Monthly Profit:  $" +monthlyProfit);
     }
-    
+   
 }
+
